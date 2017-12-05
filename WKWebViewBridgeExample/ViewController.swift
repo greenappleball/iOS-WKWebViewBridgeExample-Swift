9d84ba4fca38e2785aab8a382c29b3697e8dc175
Dmitri Petrishin <green.apple.ball@gmail.com>
2017-12-05 15:34:14 +0200

    renewed

--- WKWebViewBridgeExample/ViewController.swift
+++ WKWebViewBridgeExample/ViewController.swift
@@ -12,24 +12,24 @@ import WebKit
 class ViewController: UIViewController, WKScriptMessageHandler, WKNavigationDelegate {
     var webView: WKWebView?
     var buttonClicked:Int = 0
-    var colors:[String] = ["0xff00ff","#ff0000","#ffcc00","#ccff00","#ff0033","#ff0099","#cc0099","#0033ff","#0066ff","#ffff00","#0000ff","#0099cc"];
+    var colors:[String] = ["0xff00ff","#ff0000","#ffcc00"];
     var webConfig:WKWebViewConfiguration {
         get {
             
                 // Create WKWebViewConfiguration instance
-                var webCfg:WKWebViewConfiguration = WKWebViewConfiguration()
+                let webCfg:WKWebViewConfiguration = WKWebViewConfiguration()
                 
                 // Setup WKUserContentController instance for injecting user script
-                var userController:WKUserContentController = WKUserContentController()
+                let userController:WKUserContentController = WKUserContentController()
                 
                 // Add a script message handler for receiving  "buttonClicked" event notifications posted from the JS document using window.webkit.messageHandlers.buttonClicked.postMessage script message
-                userController.addScriptMessageHandler(self, name: "buttonClicked")
+            userController.add(self, name: "buttonClicked")
             
                 // Get script that's to be injected into the document
                 let js:String = buttonClickEventTriggeredScriptToAddToDocument()
                 
                 // Specify when and where and what user script needs to be injected into the web document
-                var userScript:WKUserScript =  WKUserScript(source: js, injectionTime: WKUserScriptInjectionTime.AtDocumentEnd, forMainFrameOnly: false)
+            let userScript:WKUserScript =  WKUserScript(source: js, injectionTime: WKUserScriptInjectionTime.atDocumentEnd, forMainFrameOnly: false)
             
                 // Add the user script to the WKUserContentController instance
                 userController.addUserScript(userScript)
@@ -54,21 +54,20 @@ class ViewController: UIViewController, WKScriptMessageHandler, WKNavigationDele
         
     }
 
-    override func viewDidAppear(animated: Bool) {
+    override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
                 
         // Load the HTML document
         loadHtml()
     }
     
-    override func viewDidDisappear(animated: Bool) {
+    override func viewDidDisappear(_ animated: Bool) {
         super.viewDidDisappear(animated)
         
-        var fileName:String =  String("\( NSProcessInfo.processInfo().globallyUniqueString)_TestFile.html")
+        let fileName:String =  String("\( ProcessInfo.processInfo.globallyUniqueString)_TestFile.html")
         
-        var error:NSError?
-        var tempHtmlPath:String =  NSTemporaryDirectory().stringByAppendingPathComponent(fileName)
-        NSFileManager.defaultManager().removeItemAtPath(tempHtmlPath, error: &error)
+        let tempHtmlPath:String =  NSTemporaryDirectory().appending(fileName)
+        try? FileManager.default.removeItem(atPath: tempHtmlPath)
         
         webView = nil
      
@@ -80,35 +79,34 @@ class ViewController: UIViewController, WKScriptMessageHandler, WKNavigationDele
     }
     
     // WKNavigationDelegate
-    func webView(webView: WKWebView, didFinishNavigation navigation: WKNavigation!) {
-        NSLog("%s", __FUNCTION__)
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        NSLog("%s", #function)
     }
     
-    func webView(webView: WKWebView, didFailNavigation navigation: WKNavigation!, withError error: NSError) {
-        NSLog("%s. With Error %@", __FUNCTION__,error)
-        showAlertWithMessage("Failed to load file with error \(error.localizedDescription)!")
+    func webView(_ webView: WKWebView, didFail navigation: WKNavigation!, withError error: Error) {
+        debugPrint("%s. With Error %@", #function,error)
+        showAlertWithMessage(message: "Failed to load file with error \(error.localizedDescription)!")
     }
     
     
     // File Loading
     func loadHtml() {
         // NOTE: Due to a bug in webKit as of iOS 8.1.1 we CANNOT load a local resource when running on device. Once that is fixed, we can get rid of the temp copy
-        let mainBundle:NSBundle = NSBundle(forClass: ViewController.self)
-        var error:NSError?
+        let mainBundle:Bundle = Bundle(for: ViewController.self)
         
-        var fileName:String =  String("\( NSProcessInfo.processInfo().globallyUniqueString)_TestFile.html")
+        let fileName:String =  String("\( ProcessInfo.processInfo.globallyUniqueString)_TestFile.html")
         
-        var tempHtmlPath:String? = NSTemporaryDirectory().stringByAppendingPathComponent(fileName)
+        let tempHtmlPath:String? = NSTemporaryDirectory().appending(fileName)
         
-        if let htmlPath = mainBundle.pathForResource("TestFile", ofType: "html") {
-            NSFileManager.defaultManager().copyItemAtPath(htmlPath, toPath: tempHtmlPath!, error: &error)
+        if let htmlPath = mainBundle.path(forResource: "TestFile", ofType: "html") {
+            try? FileManager.default.copyItem(atPath: htmlPath, toPath: tempHtmlPath!)
             if tempHtmlPath != nil {
-                let requestUrl = NSURLRequest(URL: NSURL(fileURLWithPath: tempHtmlPath!)!)
-                webView?.loadRequest(requestUrl)
+                let requestUrl = URLRequest(url: URL(fileURLWithPath: tempHtmlPath!) as URL)
+                webView?.load(requestUrl as URLRequest)
             }
         }
         else {
-           showAlertWithMessage("Could not load HTML File!")
+            showAlertWithMessage(message: "Could not load HTML File!")
         }
 
     }
@@ -118,20 +116,20 @@ class ViewController: UIViewController, WKScriptMessageHandler, WKNavigationDele
         // Script: When window is loaded, execute an anonymous function that adds a "click" event handler function to the "ClickMeButton" button element. The "click" event handler calls back into our native code via the window.webkit.messageHandlers.buttonClicked.postMessage call
         var script:String?
         
-        if let filePath:String = NSBundle(forClass: ViewController.self).pathForResource("ClickMeEventRegister", ofType:"js") {
+        if let filePath:String = Bundle(for: ViewController.self).path(forResource: "ClickMeEventRegister", ofType:"js") {
         
-            script = String (contentsOfFile: filePath, encoding: NSUTF8StringEncoding, error: nil)
+            script = try? String(contentsOfFile: filePath, encoding: .utf8)
         }
         return script!;
         
     }
     
     // WKScriptMessageHandler Delegate
-    func userContentController(userContentController: WKUserContentController, didReceiveScriptMessage message: WKScriptMessage) {
+    func userContentController(_ userContentController: WKUserContentController, didReceive message: WKScriptMessage) {
         
         if let messageBody:NSDictionary = message.body as? NSDictionary {
-            let idOfTappedButton:String = messageBody["ButtonId"] as String
-            updateColorOfButtonWithId(idOfTappedButton)
+            let idOfTappedButton:String = messageBody["ButtonId"] as! String
+            updateColorOfButtonWithId(buttonId: idOfTappedButton)
         }
         
     }
@@ -144,26 +142,26 @@ class ViewController: UIViewController, WKScriptMessageHandler, WKNavigationDele
         let color:String = colors [index]
         
          // Script that changes the color of tapped button
-        var js2:String = String(format: "var button = document.getElementById('%@'); button.style.backgroundColor='%@';", buttonId,color)
+        let js2:String = String(format: "var button = document.getElementById('%@'); button.style.backgroundColor='%@';", buttonId,color)
         
         webView?.evaluateJavaScript(js2, completionHandler: { (AnyObject, NSError) -> Void in
-            NSLog("%s", __FUNCTION__)
+            debugPrint(#function)
 
         })
     }
     
     // Helper
     func showAlertWithMessage(message:String) {
-        let alertAction:UIAlertAction = UIAlertAction(title: "Cancel", style: UIAlertActionStyle.Cancel) { (UIAlertAction) -> Void in
-            self.dismissViewControllerAnimated(true, completion: { () -> Void in
+        let alertAction:UIAlertAction = UIAlertAction(title: "Cancel", style: UIAlertActionStyle.cancel) { (UIAlertAction) -> Void in
+            self.dismiss(animated: true, completion: { () -> Void in
                 
             })
         }
         
-        let alertView:UIAlertController = UIAlertController(title: nil, message: message, preferredStyle: UIAlertControllerStyle.Alert)
+        let alertView:UIAlertController = UIAlertController(title: nil, message: message, preferredStyle: UIAlertControllerStyle.alert)
         alertView.addAction(alertAction)
         
-        self.presentViewController(alertView, animated: true, completion: { () -> Void in
+        self.present(alertView, animated: true, completion: { () -> Void in
             
         })
     }
